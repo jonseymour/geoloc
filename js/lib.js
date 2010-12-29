b3f5fbd4dfce50fb3ef46cdc78c756fb53700b39
Jon Seymour <jon.seymour@gmail.com>
2010-12-30 09:37:09 +1100

    Introduce lib.js.

---
+++ js/lib.js
@@ -0,0 +1,32 @@
+//
+// assumes center, zoom and locations are defined elsewhere
+//
+function map_initialize() {
+
+    var latlng = new google.maps.LatLng(center.location.latitude, center.location.longitude);
+
+    var myOptions = {
+      zoom: zoom,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    for (i = 0; i < locations.length; i++)
+    {
+        var title = locations[i].mac_address + " - " + locations[i].ssid + " - " + locations[i].accessed_at;
+
+        if (locations[i].location.address != undefined) {
+            title = title + " - " + locations[i].location.address.street_number + ", " + locations[i].location.address.street
+        }
+ 
+	new google.maps.Marker({
+	      	position: new google.maps.LatLng(locations[i].location.latitude, locations[i].location.longitude), 
+      		map: map, 
+      		"title": title
+	  })
+    } 
+    
+}
+
