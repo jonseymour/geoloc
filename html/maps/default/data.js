fd7d6305cb2c6f8994535d8974254b653330038f
Jon Seymour <jon.seymour@gmail.com>
2010-12-30 10:03:33 +1100

    Add map template to html directory.

---
+++ html/maps/default/data.js
@@ -0,0 +1,26 @@
+var zoom=14;
+
+var center = 
+{
+    "access_token": "2:SRcgTWwrQTM2aJ1g:8BSFoVGMG5Hl3una", 
+    "accessed_at": "20101230T080334", 
+    "location": {
+        "accuracy": 140000.0, 
+        "address": {
+            "city": "Sydney", 
+            "country": "Australia", 
+            "country_code": "AU", 
+            "postal_code": "2000", 
+            "region": "New South Wales", 
+            "street": "George St", 
+            "street_number": "354-360"
+        }, 
+        "latitude": -33.867139, 
+        "longitude": 151.207114
+    }, 
+    "mac_address": "FF:FF:FF:FF:FF:FF", 
+    "ssid": ""
+};
+
+var locations = [
+];
